c3718556dde138c74c91b2be1a251fa0193683e8
Vanapiton <65999898+vanapiton@users.noreply.github.com>
2026-09-19 01:27:56 +0300

    Clean TranslationStorageMixin

--- src/main/java/fi/_1up/coolbelt/mixin/lang/TranslationStorageMixin.java
+++ src/main/java/fi/_1up/coolbelt/mixin/lang/TranslationStorageMixin.java
@@ -4,6 +4,7 @@ import net.fabricmc.loader.api.FabricLoader;
 import net.minecraft.client.resource.language.TranslationStorage;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
@@ -11,17 +12,20 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import java.io.IOException;
 import java.util.Properties;
 
+import static fi._1up.coolbelt.Coolbelt.LOGGER;
+
 @Mixin(TranslationStorage.class)
 public class TranslationStorageMixin {
     @Shadow private Properties translations;
+    @Unique private static final String TRANSLATIONS_PATH = "/assets/coolbelt/stationapi/lang/en_US.lang";
 
     @Inject(method = "<init>", at = @At(value = "INVOKE", target = "Ljava/util/Properties;load(Ljava/io/InputStream;)V", ordinal = 1, shift = At.Shift.AFTER))
     public void init(CallbackInfo ci) {
         if (!FabricLoader.getInstance().isModLoaded("stationapi")) {
             try {
-                translations.load(TranslationStorage.class.getResourceAsStream("/assets/coolbelt/stationapi/lang/en_US.lang"));
+                translations.load(TranslationStorage.class.getResourceAsStream(TRANSLATIONS_PATH));
             } catch (IOException err) {
-                System.err.println("Failed to load Coolbelt translations!");
+                LOGGER.error("Failed to load Coolbelt translations: %s", err.getMessage());
             }
         }
     }
